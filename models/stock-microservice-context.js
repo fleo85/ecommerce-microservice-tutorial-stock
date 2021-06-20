d0de2e9595b423dbd7d86e29ff1600627e397ff3
fleo85 <francescoleotta@hotmail.com>
2021-06-20 17:33:48 +0200

    use env variables for postgres

--- models/stock-microservice-context.js
+++ models/stock-microservice-context.js
@@ -2,9 +2,12 @@ const ProductModel = require('./product.js')
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+var database = process.env.POSTGRES_HOST || config.database
+var username = process.env.POSTGRES_USERNAME || config.username
+var password = process.env.POSTGRES_PASSWORD || config.password
 
 var sequelize = require('sequelize');
-const sequelizeconn = new sequelize(config.database, config.username, config.password, config);
+const sequelizeconn = new sequelize(database, username, password, config);
 
 const Product = ProductModel(sequelizeconn, sequelize);
 
